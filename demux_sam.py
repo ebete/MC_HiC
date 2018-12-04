dc8e0df9dd55487b08fdd7a20f102d8a64716dd1
Thom Griffioen <thomgriffioen@gmail.com>
2018-12-04 13:36:35 +0100

    Also display the number of mapped reads per sample

--- demux_sam.py
+++ demux_sam.py
@@ -8,18 +8,19 @@ import os
 import pysam
 
 
-def demux_sam(sam_input, outdir):
+def demux_sam(sam_input, map_counts, outdir="./", sam_region="."):
     input_name = os.path.basename(sam_input).partition(".")[0]
     demuxed_reads = {}
     with pysam.AlignmentFile(sam_input, "r") as samfile:
         logging.info("Demuxing alignment file %s ...", sam_input)
-        for read in samfile.fetch():
+        for read in samfile.fetch(region=sam_region):
             read_metadata = {}
             for x in str(read.qname).split(";"):
                 read_metadata[x.split(":")[0]] = x.split(":")[1]
 
             outfile = os.path.join(outdir, "{}_{}.bam".format(input_name, read_metadata.get("Fq.Id", "Unknown")))
             demuxed_reads.setdefault(outfile, []).append(read)
+            map_counts.setdefault(read_metadata.get("Fq.Id", "Unknown"), []).append(read_metadata.get("Rd.Id", "-1"))
 
         for outfile, reads in demuxed_reads.items():
             logging.info("Writing %d reads to %s ...", len(reads), outfile)
@@ -39,8 +40,15 @@ if __name__ == "__main__":
                         nargs="+")
     parser.add_argument("--output", "-o", help="Output directory of the de-multiplexed alignment files", metavar="DIR",
                         action="store", type=str, default="./")
+    parser.add_argument("--region", "-r", help="Limit read exporting to specific region", metavar="REGION",
+                        action="store", type=str, default=".")
     args = parser.parse_args()
 
+    count_per_file = {}
     for globfile in args.input_sam:
         for samfile in glob.iglob(globfile):
-            demux_sam(samfile, args.output)
+            demux_sam(samfile, count_per_file, args.output, sam_region=args.region)
+
+    for fq_id, rd_ids in count_per_file.items():
+        counts = len(set(rd_ids))
+        logging.info("Unique reads in %s: %d", fq_id, counts)
