3fe849671830c0cab988e0d6754390d9fc2260c8
Thom Griffioen <thomgriffioen@gmail.com>
2018-12-05 16:41:10 +0100

    Heatmap no longer normalised and colors are more discernable

--- create_heatmap.R
+++ create_heatmap.R
@@ -13,18 +13,21 @@ pdf("create_heatmap.pdf", width = 11.69, height = 8.27, pointsize = 6)
 relfreq <- function(x) { x / sum(x)}
 
 # load data and normalise
-df <- read.csv("/home/thom/mc_hic/mc_4c/demux/result.pdf.csv")[, - 1]
+df <- read.csv("/home/thom/mc_hic/mc_4c_digested/demux/result.pdf.csv")[- c(1, 2), - 1]
 mtx <- t(data.matrix(df))
-mtx.norm <- t(apply(mtx, c(1), relfreq))
+#mtx.norm <- t(apply(mtx, c(1), relfreq))
 
 # plot dendrogram
 col_palette <- colorRampPalette(brewer.pal(n = 9, name = "Reds"))
-heatmap.2(mtx.norm,
+col_palette <- colorRampPalette(c("red", "white", "green"))
+
+heatmap.2(mtx,
 dendrogram = "row",
+hclustfun = function(x) {hclust(x, method = "average")},
 Colv = F,
 margins = c(4, 12),
-col = col_palette(100),
-tracecol = "gray",
+col = col_palette(15),
+tracecol = NULL,
 cellnote = mtx,
 notecol = "black"
 )
