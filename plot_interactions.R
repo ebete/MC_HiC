2ea01c4b6ca2b54a071b1483bbbd888a39e8894e
Thom Griffioen <thomgriffioen@gmail.com>
2018-12-20 16:29:39 +0100

    Make plot title bold

--- plot_interactions.R
+++ plot_interactions.R
@@ -9,7 +9,7 @@ suppressPackageStartupMessages({
   library(cowplot)
 })
 
-interactions <- read.csv("/data0/thom/temp/bwa_run.bam_interactions.csv", sep = ";")
+interactions <- read.csv("/data0/thom/temp/bwa_run.bam.csv_interactions.csv", sep = ";")
 
 # interaction map
 g <- ggplot(interactions, aes(x = x, y = y)) +
@@ -18,7 +18,7 @@ g <- ggplot(interactions, aes(x = x, y = y)) +
   scale_x_continuous(position = "top", labels = function(x) { sprintf("%.0f Mb", x / 1e6)}) +
   xlab("chr7") +
   ylab("chr7") +
-  theme(plot.title = element_text(hjust = 0.5), legend.position = "bottom", axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
+  theme(plot.title = element_text(hjust = 0.5, face = "bold"), legend.position = "bottom", axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
 plt1 <- g +
   geom_bin2d(bins = 500) +
   scale_fill_continuous(type = "viridis", limits = c(0, 30))
@@ -28,7 +28,7 @@ genemap <- autoplot(TxDb.Mmusculus.UCSC.mm9.knownGene, which = range(GRanges(Rle
 plt2 <- genemap@ggplot +
   ggtitle("chr7 MC-4C interaction map (105 Mb - 115 Mb)") +
   scale_x_continuous(labels = function(x) { sprintf("%.0f Mb", x / 1e6)}) +
-  theme(plot.title = element_text(hjust = 0.5), axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
+  theme(plot.title = element_text(hjust = 0.5, face = "bold"), axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
 
 # plot both
 plot_grid(plt2, plt1, ncol = 1, align = "h", axis = "t", rel_heights = c(1 / 5, 4 / 5))
