12e25f11ee0b8313b8a66c80327e5b954e951dde
Thom Griffioen <thomgriffioen@gmail.com>
2019-02-26 16:04:44 +0100

    Convert the alignment CSV to a fragments per read count matrix

---
+++ fragments_to_matrix.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import argparse
+import csv
+import logging
+
+import utils
+
+
+def get_fragment_counts(csv_file):
+    logging.info("Extracting fragmentation of %s ...", csv_file)
+    fragments_counts = dict()
+
+    with open(csv_file, "r", newline="") as fin:
+        handle = csv.reader(fin, delimiter=";")
+        next(handle)
+        for row in handle:
+            rdid = row[0]
+            fragments_counts[rdid] = fragments_counts.get(rdid, 0) + 1
+
+    fragments_per_read = dict()
+    for fragments in fragments_counts.values():
+        fragments_per_read[fragments] = fragments_per_read.get(fragments, 0) + 1
+
+    return fragments_per_read
+
+
+def create_matrix(counts, limit=5):
+    print("file", "\t".join([str(i) for i in range(1, limit + 1)]), sep="\t")
+    for fname, fragment_count in counts.items():
+        row = fname
+        for i in range(1, limit + 1):
+            row += '\t' + str(fragment_count.get(i, 0))
+        print(row)
+
+
+if __name__ == '__main__':
+    utils.init_logger()
+
+    # get command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_csv", help="Input CSV files.", metavar="CSV", action="store", type=str,
+                        nargs="+")
+    args = parser.parse_args()
+
+    counts_per_file = {}
+    max_count = 0
+    for csvfile in utils.glob_all_files(args.input_csv):
+        counts = get_fragment_counts(csvfile)
+        counts_per_file[csvfile] = counts
+        max_count = max(max_count, max(counts.keys()))
+    create_matrix(counts_per_file, max_count)
