329f4c9ef17e33f3f472313b00904bf3aedd9297
Thom Griffioen <thomgriffioen@gmail.com>
2019-02-26 16:02:55 +0100

    Create a single heatmap for the alignment comparison

--- create_heatmap.R
+++ create_heatmap.R
@@ -7,63 +7,37 @@ suppressPackageStartupMessages({
   library(abind)
 })
 
-#par(bg = 'black', fg = 'white')
-# functions
-relfreq <- function(x) { x / sum(x)}
-
 # load data
-df <- read.csv("/home/thom/mc_hic/mc_4c_digested/demux/result.pdf.csv")[- 1 : - 3, - 1]
-mtx <- t(data.matrix(df))
+df <- read.csv("/data0/thom/mc4c_aligntest2/all_fragments.csv", sep = "\t", header = T)
+mtx <- data.matrix(df[, - 1])
+row.names(mtx) <- df[, 1]
+colnames(mtx) <- 1 : ncol(mtx)
+# remove first few columns
+merge_from <- 8
+merged_tail <- apply(mtx[, merge_from : ncol(mtx)], 1, sum)
+mtx.subset <- cbind(mtx[, - (merge_from : ncol(mtx))], "8+" = merged_tail)
+mtx.subset <- mtx.subset[, - (1 : 2)]
 
-# infer score
+# sort based on total mapped fragments
 mtx.scores <- matrix(mtx[, 1])
-row.names(mtx.scores) <- row.names(mtx)
 for (i in 2 : ncol(mtx)) {
   mtx.scores <- mtx.scores + mtx[, i] * i
 }
-# sort based on score
-mtx <- mtx[order(mtx.scores[, 1], decreasing = T),]
+mtx.subset <- mtx.subset[order(mtx.scores[, 1], decreasing = T),]
 
-#mtx.norm <- t(apply(mtx, c(1), relfreq))
-# color labels based on sample source
-mtx.fac <- rep("black", nrow(mtx))
-for (i in 1 : nrow(mtx)) {
-  source <- strsplit(row.names(mtx)[i], "_", fixed = T)[[1]]
-  source_dataset <- source[length(source)]  # get data set
-  source_conf <- paste(source[- length(source)], collapse = "_")  # get alignment conf
-  mtx.fac[i] <- source_dataset
-}
-mtx.fac <- as.factor(mtx.fac)
-mtx.col <- colorRampPalette(brewer.pal(n = length(levels(mtx.fac)), name = "Set1"))(length(levels(mtx.fac)))[as.numeric(mtx.fac)]
-
-rankings <- data.frame(rank = 1 : 13)
-run <- 1
-for (l in levels(mtx.fac)) {
-  ranked <- data.frame(row.names(mtx[mtx.fac == l,]))
-  colnames(ranked) <- sprintf("run_%d", as.integer(run))
-  rankings <- cbind(rankings, ranked)
-  run <- run + 1
-}
-
-
-# plot dendrogram
-col_palette <- colorRampPalette(brewer.pal(n = 9, name = "PuOr"))
-#col_palette <- colorRampPalette(c("orange", "white", "cyan"))
-
-pdf("heatmaps.pdf", width = 11.7, height = 8.27, pointsize = 12, colormodel = "cmyk")
-for (l in levels(mtx.fac)) {
-  heatmap.2(mtx[mtx.fac == l,],
+# plot heatmap
+col_palette <- colorRampPalette(brewer.pal(n = 3, name = "PuOr"))
+heatmap.2(
+mtx.subset,
   dendrogram = "none",
-  hclustfun = function(x) {hclust(x, method = "average")},
   Colv = F,
   Rowv = F,
+key = T,
+tracecol = NULL,
+cellnote = mtx.subset,
+notecol = "black",
   margins = c(4, 20),
   col = col_palette(15),
-  tracecol = NULL,
-  cellnote = mtx[mtx.fac == l,],
-  notecol = "black",
-  #    colRow = mtx.col[mtx.fac == l],
-  colCol = "black"
-  )
-}
-dev.off()
+main = "Mapped fragments per read for different alignments",
+xlab = "Fragments per read"
+)
