b3b4cdf76409cd08260569bd88f4c398aa39dd75
Thom Griffioen <thomgriffioen@gmail.com>
2019-02-27 10:13:16 +0100

    Use utils helper script

--- extract_chimeric.py
+++ extract_chimeric.py
@@ -7,21 +7,22 @@ from tempfile import NamedTemporaryFile
 
 import pysam
 
+import utils
+
 
 def parse_chimeric(sam_input, bam_output, min_mappings):
     with NamedTemporaryFile("wb", buffering=0) as tmp:
         logging.info("Created temporary BAM file %s", tmp.name)
         with pysam.AlignmentFile(sam_input) as sam, \
             pysam.AlignmentFile(tmp, "wb", template=sam) as fout:
-            grouped_reads = {}
+            grouped_reads = dict()
 
             logging.info("Reading SAM file ...")
             for read in sam:
                 # fragment id (Fr.Id) should be ignored
-                read_metadata = {}
-                for x in str(read.qname).split(";"):
-                    read_metadata[x.split(":")[0]] = x.split(":")[1]
-                rdid = "{}_{}".format(read_metadata["Fq.Id"], read_metadata["Rd.Id"])
+                read_metadata = utils.read_header_to_dict(read.qname)
+                rdid = utils.make_read_id(read_metadata)
+
                 grouped_reads.setdefault(rdid, list()).append(read)
 
             logging.info("Writing chimeric reads to temporary BAM")
@@ -37,7 +38,7 @@ def parse_chimeric(sam_input, bam_output, min_mappings):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(message)s")
+    utils.init_logger()
 
     # Get command argument
     parser = argparse.ArgumentParser()
