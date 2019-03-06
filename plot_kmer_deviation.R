eb37592ff1c8ad9bd40bd5c0923fee65fc1cf5ba
Thom Griffioen <thomgriffioen@gmail.com>
2019-03-06 17:10:07 +0100

    Split the plot into facets for each 2-mer

--- plot_kmer_deviation.R
+++ plot_kmer_deviation.R
@@ -5,74 +5,49 @@ suppressPackageStartupMessages({
   library(reshape2)
   library(scales)
   library(RColorBrewer)
-  library(latex2exp)
   library(ggpubr)
+  library(dplyr)
 })
 
 
-mapped_2mer <- read.csv("/data0/thom/mergemap_splitmap/mappable_2mer.csv", "\t", sep = "\t", header = T)
-unmapped_2mer <- read.csv("/data0/thom/mergemap_splitmap/unmapped_2mer.csv", sep = "\t", header = T)
-
-mapped_2mer$rel_count <- mapped_2mer$count / sum(mapped_2mer$count)
-unmapped_2mer$rel_count <- unmapped_2mer$count / sum(unmapped_2mer$count)
+reference_2mer <- read.csv("/data0/thom/mc4c_window/mc4c_window_2mer.csv", "\t", sep = "\t", header = T) %>%
+mutate(rel_count = count / sum(count))
+mapped_2mer <- read.csv("/data0/thom/mergemap_splitmap/mappable_2mer.csv", "\t", sep = "\t", header = T) %>%
+mutate(rel_count = count / sum(count) - reference_2mer$rel_count)
+unmapped_2mer <- read.csv("/data0/thom/mergemap_splitmap/unmapped_2mer.csv", sep = "\t", header = T) %>%
+mutate(rel_count = count / sum(count) - reference_2mer$rel_count)
 
 counts_2mer <- data.frame(
 first = mapped_2mer$first,
 second = mapped_2mer$second,
 seq = as.factor(paste0(as.character(mapped_2mer$first), as.character(mapped_2mer$second))),
 mapped = mapped_2mer$rel_count,
-unmapped = unmapped_2mer$rel_count
+unmapped = unmapped_2mer$rel_count,
+reference = reference_2mer$rel_count
 )
-counts_2mer.m <- melt(counts_2mer, id.vars = c("first", "second", "seq"))
-counts_2mer.m$deviation <- counts_2mer.m$value - 1 / 4 ^ 2
+counts_2mer.m <- melt(counts_2mer, id.vars = c("first", "second", "seq")) %>%
+mutate(deviation_from_expected = value - 4 ^ - 2)
+max_count <- max(abs(counts_2mer.m[counts_2mer.m$variable != "reference",]$value))
 
-ggplot(counts_2mer.m, aes(x = seq, y = deviation, fill = variable)) +
-  geom_col(position = "dodge") +
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 90)) +
-  scale_y_continuous(limits = c(- max(abs(counts_2mer.m$deviation)), max(abs(counts_2mer.m$deviation)))) +
-  scale_fill_brewer(palette = "Dark2") +
-  labs(
-  title = "2-mer rate difference betweeen mappable and unmappable reads",
-  x = "2-mer",
-  y = TeX("Deviation from expected ($4^{-2}$)")
-  )
-
-first <- ggplot(counts_2mer.m, aes(x = second, y = deviation, fill = variable)) +
+ggplot(subset(counts_2mer.m, variable != "reference"), aes(x = variable, y = value, fill = variable)) +
   geom_col(position = "identity", alpha = 0.5) +
-  facet_grid(. ~ first, labeller = label_both) +
-  scale_y_continuous(limits = c(- max(abs(counts_2mer.m$deviation)), max(abs(counts_2mer.m$deviation))), labels = scales::percent) +
-  geom_hline(yintercept = 0) +
-  theme_classic() +
+  facet_grid(second ~ first, labeller = label_both) +
+#  geom_text(data = subset(counts_2mer.m, variable == "reference"), aes(x=1,y=-0.04, label = sprintf("%.1f%%", value*100))) +
+  scale_y_continuous(labels = scales::percent, breaks = pretty_breaks()) +
+  geom_hline(yintercept = 0, linetype = "dotted") +
+  theme_pubr(legend = "top") +
   theme(
-  axis.text.x = element_text(angle = 90),
-  panel.spacing.x = unit(0, units = "mm"),
+  plot.title = element_text(hjust = 0.5, face = "bold"),
+  axis.text.x = element_blank(),
+  axis.ticks.x = element_blank(),
+  axis.title.x = element_blank(),
+  panel.spacing = unit(0, units = "mm"),
   strip.background = element_rect(fill = "gray98", colour = "black"),
   panel.border = element_rect(colour = "black", fill = NA, size = 1)
   ) +
   scale_fill_brewer(palette = "Set1") +
   labs(
-  x = "Second nucleotide",
-  y = TeX("Deviation from expected ($4^{-2}$)")
+  x = "",
+  y = "Deviation from reference",
+  title = "Nucleotide association between mapped and unmapped reads"
   )
-
-second <- ggplot(counts_2mer.m, aes(x = first, y = deviation, fill = variable)) +
-  geom_col(position = "identity", alpha = 0.5) +
-  facet_grid(. ~ second, labeller = label_both) +
-  scale_y_continuous(limits = c(- max(abs(counts_2mer.m$deviation)), max(abs(counts_2mer.m$deviation))), labels = scales::percent) +
-  geom_hline(yintercept = 0) +
-  theme_classic() +
-  theme(
-  axis.text.x = element_text(angle = 90),
-  panel.spacing.x = unit(0, units = "mm"),
-  strip.background = element_rect(fill = "gray98", colour = "black"),
-  panel.border = element_rect(colour = "black", fill = NA, size = 1)
-  ) +
-  scale_fill_brewer(palette = "Set1") +
-  labs(
-  x = "First nucleotide",
-  y = TeX("Deviation from expected ($4^{-2}$)")
-  )
-
-combined <- ggarrange(first, second, nrow = 2, labels = c("(1)", "(2)"), hjust = 0, align = "h", common.legend = T, legend = "top")
-annotate_figure(combined, top = "Nucleotide association difference betweeen mappable and unmappable reads")
